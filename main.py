c645bd89347a3ee35fbdbc8e16dd85d5aff3ff72
Leandro G <leagurru@gmail.com>
2024-09-02 09:57:13 -0300

    inicial commit

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
+import sys
 from youtube_transcript_api import YouTubeTranscriptApi
-video_id="OuuO3lgh5oo"
+import requests
 
-# Replace with your proxy server details
-proxy_server = "http://127.0.0.1:9090"
+# video_id="OuuO3lgh5oo"
 
+if len(sys.argv) != 2:
+    print("Usage: python script.py <video_id>")
+    sys.exit(1)
+
+video_id = sys.argv[1]
+print(f"Procesando video id: {video_id}")
+
+
+# Set the SOCKS4 proxy server details
+proxy_server = "127.0.0.1"
+proxy_port = "9090"
+
+# Configure the proxies
 proxies = {
-    "http": proxy_server,
-    "https": proxy_server,
+    "http": f"http://{proxy_server}:{proxy_port}",
+    "https": f"http://{proxy_server}:{proxy_port}",
 }
 
+requests.proxies = proxies
 
-def transcriptor(video_id):
-    # Use a breakpoint in the code line below to debug your script.
-    # print(f'Hi, {video_id}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def transcriptor(str_video_id, str_proxies):
     try:
 
         # Get the transcript using the proxy server
-        transcript = YouTubeTranscriptApi.get_transcript(video_id, proxies=proxies)
+        transcript = YouTubeTranscriptApi.get_transcript(str_video_id, str_proxies)
+        # transcript = YouTubeTranscriptApi.get_transcript(str_video_id, proxies=proxies)
 
         # Print the transcript
         for entry in transcript:
@@ -29,6 +43,5 @@ def transcriptor(video_id):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    transcriptor(video_id)
+    transcriptor(video_id, requests.proxies)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
