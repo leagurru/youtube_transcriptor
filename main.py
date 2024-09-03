49aa2f80923dcdcafb722b0fd44ea110949f8a9a
Leandro G <leagurru@gmail.com>
2024-09-03 10:20:10 -0300

    script funcionando

--- main.py
+++ main.py
@@ -1,47 +1,34 @@
 import sys
 from youtube_transcript_api import YouTubeTranscriptApi
-import requests
-
-# video_id="OuuO3lgh5oo"
 
 if len(sys.argv) != 2:
-    print("Usage: python script.py <video_id>")
+    print("Uso desde línea de comandos: python script.py <video_id>")
     sys.exit(1)
 
 video_id = sys.argv[1]
 print(f"Procesando video id: {video_id}")
+archivo = sys.argv[1]+".txt"
 
 
-# Set the SOCKS4 proxy server details
-proxy_server = "127.0.0.1"
-proxy_port = "9090"
-
-# Configure the proxies
-proxies = {
-    "http": f"http://{proxy_server}:{proxy_port}",
-    "https": f"http://{proxy_server}:{proxy_port}",
-}
-
-requests.proxies = proxies
-
-
-def transcriptor(str_video_id, str_proxies):
+def transcriptor(str_video_id, str_archivo):
     try:
 
-        # Get the transcript using the proxy server
-        transcript = YouTubeTranscriptApi.get_transcript(str_video_id, str_proxies)
-        # transcript = YouTubeTranscriptApi.get_transcript(str_video_id, proxies=proxies)
+        transcript = YouTubeTranscriptApi.get_transcript(str_video_id, languages=['es'])
 
-        # Print the transcript
+        texto = ""
+
+        # obtengo transcripción línea a línea
         for entry in transcript:
-            print(entry['text'])
+            texto += entry['text'] + "\n"
+
+        # envío la transcripción a un archivo con el video_id.txt
+        with open(str_archivo, "w") as file:
+            file.write(texto)
 
     except Exception as e:
         print(f"Error: {e}")
 
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    transcriptor(video_id, requests.proxies)
+    transcriptor(video_id, archivo)
 
