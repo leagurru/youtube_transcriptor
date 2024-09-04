7307a1dbe944ba42ac045451047d5caf50c31ed2
Leandro G <leagurru@gmail.com>
2024-09-04 13:13:53 -0300

    traduce opcionalmente

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-Script en python 3.12 para obtener la transcripción en el lenguaje ES de un video en youtube.
-Se invoca desde la línea de comandos (indicando el id del video como parámetro). 
+Script en python 3.12 para obtener la transcripción en el lenguaje ES de un video en youtube
+si se indica otro parámetro se intenta realizar la traducción al lenguaje indicado
+Se invoca desde la línea de comandos (indicando el id del video y el lenguaje de la transcripción 'es' como parámetro)
+opcionalmente se puede indicar un lenguaje para traducir la transcripción
 
 Ejemplo:
-python3 youtube_transcriptor.py OuuO3l37653o
-
+python3 youtube_transcriptor.py OuuO3l37653o es en
+python3 youtube_transcriptor.py OuuO3l37653o es 
 
 Instalación:
 virtualenv venv
--- youtube_transcriptor.py
+++ youtube_transcriptor.py
@@ -4,22 +4,51 @@ from youtube_transcript_api import YouTubeTranscriptApi
 # Video ejemplo:  https://www.youtube.com/watch?v=OuuO3lgh5oo
 # video_id=OuuO3lgh5oo
 
-def transcriptor(str_video_id, str_archivo, str_language):
+def transcriptor(str_video_id, str_language_transcripcion, str_language_traduccion):
     try:
 
-        transcript = YouTubeTranscriptApi.get_transcript(str_video_id, languages=[str_language])
+        # retrieve the available transcripts
+        transcript_list = YouTubeTranscriptApi.list_transcripts(str_video_id)
+        transcript = transcript_list.find_transcript([str_language_transcripcion])
+
+        # transcripcion en el lenguaje original
+        transcription = YouTubeTranscriptApi.get_transcript(str_video_id, languages=[str_language_transcripcion])
 
         texto = ""
 
         # obtengo transcripción línea a línea
-        for entry in transcript:
+        for entry in transcription:
             texto += entry['text'] + "\n"
 
-        # envío la transcripción a un archivo con el video_id.txt
-        with open(str_archivo, "w") as file:
+        str_archivo_transcripcion = str_language_transcripcion + "-" + str_video_id + ".txt"
+        # envío la transcripción a un archivo con el lenguaje-video_id.txt
+        with open(str_archivo_transcripcion, "w") as file:
             file.write(texto)
 
-        print(f"Transcripción en el archivo {str_archivo}")
+        print(f"Transcripción en el archivo {str_archivo_transcripcion}")
+
+
+        if language_traduccion:
+            # Traducción de la transcripción
+            # fetch the actual transcript data
+            transcript.fetch()
+
+            # translating the transcript will return another transcript object
+            traduccion = transcript.translate(str_language_traduccion).fetch()
+            # print(transcript.translate(str_language_traduccion).fetch())
+
+            # obtengo la traducción línea a línea
+            texto_traducido = ""
+            for entry in traduccion:
+                texto_traducido += entry['text'] + "\n"
+
+            # envío la traducción a un archivo con el lenguaje-video_id.txt
+            str_archivo_traduccion = str_language_traduccion + "-" + str_video_id + ".txt"
+            with open(str_archivo_traduccion, "w") as file:
+                file.write(texto_traducido)
+
+            print(f"Traducción en el archivo {str_archivo_traduccion}")
+
 
     except Exception as e:
         print(f"Error: {e}")
@@ -28,15 +57,26 @@ def transcriptor(str_video_id, str_archivo, str_language):
 if __name__ == '__main__':
     
     # Verifico que haya llegado el parámetro que tendría el video_id y el language. Si no está -> exit
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         print("Uso desde línea de comandos: python main.py <video_id> <language>")
         sys.exit(1)
 
     video_id = sys.argv[1]
-    language = sys.argv[2]
-    print(f"Procesando video id: {video_id} para el lenguaje '{language}'")
-    archivo = sys.argv[1]+".txt"
+    language_transcripcion = sys.argv[2]
 
-    transcriptor(video_id, archivo, language)
+    if len(sys.argv) == 4:
+        language_traduccion = sys.argv[3]
+    else:
+        language_traduccion = False
+
+    print(language_traduccion)
+
+    print(f"Procesando video id: {video_id} para el lenguaje '{language_transcripcion}'")
+
+    if language_traduccion:
+        print(f"Se intentará traducir al lenguaje '{language_traduccion}'")
+
+    transcriptor(video_id, language_transcripcion, language_traduccion)
+
 
 
