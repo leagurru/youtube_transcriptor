3874d63dd98a181478ed3f1663328ff5851fafc0
Leandro G <leagurru@gmail.com>
2024-09-04 07:23:31 -0300

    agrego requirements.txt

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-script en python 3.12 para obtener la transcripción en el lenguaje ES de un video en youtube.
+Script en python 3.12 para obtener la transcripción en el lenguaje ES de un video en youtube.
 Se invoca desde la línea de comandos (indicando el id del video como parámetro). 
 
 Ejemplo:
-python3 main.py OuuO3l37653o
+python3 youtube_transcriptor.py OuuO3l37653o
+
+
+Instalación:
+virtualenv venv 
+pip install -r requirements.txt
--- youtube_transcriptor.py
+++ youtube_transcriptor.py
@@ -1,14 +1,8 @@
 import sys
 from youtube_transcript_api import YouTubeTranscriptApi
 
-if len(sys.argv) != 2:
-    print("Uso desde línea de comandos: python main.py <video_id>")
-    sys.exit(1)
-
-video_id = sys.argv[1]
-print(f"Procesando video id: {video_id}")
-archivo = sys.argv[1]+".txt"
-
+# Video ejemplo:  https://www.youtube.com/watch?v=OuuO3lgh5oo
+# video_id=OuuO3lgh5oo
 
 def transcriptor(str_video_id, str_archivo):
     try:
@@ -25,10 +19,22 @@ def transcriptor(str_video_id, str_archivo):
         with open(str_archivo, "w") as file:
             file.write(texto)
 
+        print(f"Transcripción en el archivo {str_archivo}")
+
     except Exception as e:
         print(f"Error: {e}")
 
 
 if __name__ == '__main__':
+    
+    # Verifico que haya llegado el parámetro que tendría el video_id. Si no está -> exit
+    if len(sys.argv) != 2:
+        print("Uso desde línea de comandos: python main.py <video_id>")
+        sys.exit(1)
+
+    video_id = sys.argv[1]
+    print(f"Procesando video id: {video_id}")
+    archivo = sys.argv[1]+".txt"
+
     transcriptor(video_id, archivo)
 
